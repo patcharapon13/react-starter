f5b1fa8473aa397d16318930229a2fa181294095
Timmy <timmy@Timmys-MacBook-Pro.local>
2023-07-21 16:26:28 +1200

    Problem is not able to solve

---
+++ src/home/Home.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import '../../src/home/Home.css';
+import App from '../App';
+
+const Home = (props) => {
+
+    return (
+        <div className="App">
+            <section class="layout">
+                <div class="grow1">1</div>
+                <div class="grow1">2</div>
+                <div class="grow1" ></div>
+                </section>
+        </div>
+    );
+};
+
+export default Home;
